d14211234fd3e079f12a3e7396106988cdc96d33
hyeonjerry <kwonhyeonjae01@gmail.com>
2024-08-10 23:40:50 +0900

    feat: data-pipeline 구현

---
+++ data-pipeline/main.py
@@ -0,0 +1,46 @@
+from os import path
+from pytz import timezone
+from datetime import datetime
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+KST = timezone("Asia/Seoul")
+today = datetime.now().astimezone(KST)
+today_string = "%04d%02d%02d" % (today.year, today.month, today.day)
+
+# 오늘 날짜의 데이터 읽기
+if not path.exists(f"data-pipeline/raw-data/{today_string}.csv"):
+    raise FileNotFoundError(f"data-pipeline/raw-data/{today_string}.csv")
+data = pd.read_csv(f"data-pipeline/raw-data/{today_string}.csv")
+
+# 20시와 05시 사이의 데이터만 필터링
+data["operated_at"] = pd.to_datetime(data["operated_at"])
+data = data[(data["operated_at"].dt.hour >= 20) | (data["operated_at"].dt.hour <= 5)]
+
+# 'avg_lightlux' 값을 0과 1 사이의 값으로 정규화
+scaler = MinMaxScaler()
+data["weight"] = scaler.fit_transform(data[["avg_lightlux"]])
+
+# # IQR의 1.5배 이상 떨어진 데이터를 이상치로 간주하고 제거
+Q1 = data["avg_lightlux"].quantile(0.25)
+Q3 = data["avg_lightlux"].quantile(0.75)
+IQR = Q3 - Q1
+
+data = data[
+    ~(
+        (data["avg_lightlux"] < (Q1 - 1.5 * IQR))
+        | (data["avg_lightlux"] > (Q3 + 1.5 * IQR))
+    )
+]
+
+# 상위 N개의 데이터 추출
+data = data.nlargest(1000, "weight")
+
+# 위도와 경도가 같은 row의 수 계산
+data["coord_key"] = data["latitude"].astype(str) + "_" + data["longitude"].astype(str)
+
+group_sizes = data.groupby("coord_key").size()
+
+data["group_size"] = data["coord_key"].map(group_sizes)
+
+data.to_csv(f"data-pipeline/processed-data/{today_string}.csv", index=False)
